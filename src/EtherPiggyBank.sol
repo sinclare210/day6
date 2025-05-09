f86c0805294b0a614809867d0025d913680129e8
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-09 01:55:21 +0100

    Starting

--- src/EtherPiggyBank.sol
+++ src/EtherPiggyBank.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+contract EtherPiggyBank {
+
+    address public owner;
+
+    address bankManager;
+
+    uint256 public balance;
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner () {
+        require (owner == msg.sender);
+        _;
+    }
+
+    function deposit () public payable {
+        require (msg.value > 0, "Amount must be greater than zero");
+        balance = balance + msg.value;
+    }
+}
