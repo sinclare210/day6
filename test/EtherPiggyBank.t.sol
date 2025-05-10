b42a7cf120f79298b43f6e54bf7263ba70b92f24
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 11:35:38 +0100

    writing function

--- test/EtherPiggyBank.t.sol
+++ test/EtherPiggyBank.t.sol
@@ -3,3 +3,14 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {EtherPiggyBank} from "../src/EtherPiggyBank.sol";
+
+
+contract EtherPiggyBankTest is Test {
+    EtherPiggyBank public etherPiggyBank;
+
+    function setUp () public {
+        eth
+    }
+
+
+}
