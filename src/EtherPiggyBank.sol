819ff55ea74c8ed0b8338d9594414dcfefe85e52
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 11:06:58 +0100

    writing function

--- src/EtherPiggyBank.sol
+++ src/EtherPiggyBank.sol
@@ -15,6 +15,7 @@ contract EtherPiggyBank {
     constructor (){
         bankManager = msg.sender;
         members.push(msg.sender);
+        registeredUsers[bankManager] = true;
     }
 
     modifier onlyBankManager () {
@@ -28,7 +29,40 @@ contract EtherPiggyBank {
     }
 
     function deposit () public payable {
-        require (msg.value > 0, "Amount must be greater than zero");
         
+        
+        
+    }
+
+    function addMember (address addy) public onlyBankManager {
+        require(msg.sender != address(0), "Address zero not allowed");
+        require(msg.sender != bankManager, "Bank manager is a member");
+        require(!registeredUsers[addy], "is a member");
+        registeredUsers[addy] = true;
+        members.push(addy);
+    }
+
+    function getMembers() public view returns (address[] memory){
+        return members;
+    }
+
+    function depositAmount () public onlyRegisteredMember payable{
+        require (msg.value > 0, "Amount must be greater than zero");
+        balance[msg.sender] += msg.value;
+    }
+
+    function getBalanceByManager (address _member) public view onlyBankManager returns (uint256){
+       require(_member != address(0), "Address zero not allowed");
+       return  balance[_member];
+    }
+
+    function withdraw (uint256 _amount) public onlyRegisteredMember {
+        require(msg.sender != address(0), "Address zero not allowed");
+        
+    }
+
+    function getBalanceByUser () public view returns (uint256){
+       require(msg.sender != address(0), "Address zero not allowed");
+       return  balance[msg.sender];
     }
 }
