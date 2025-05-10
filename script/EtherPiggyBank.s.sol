df937493e432248cb30d339640792580f75b4652
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 11:18:06 +0100

    writing function

--- script/EtherPiggyBank.s.sol
+++ script/EtherPiggyBank.s.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {EtherPiggyBank} from "../src/EtherPiggyBank.sol";
+
+contract EtherPiggyBankScript is Script {
+
+    EtherPiggyBank public etherPiggyBank;
+    
+    function setUp () public {
+
+    }
+
+    function run () public {
+        vm.startBroadcast();
+        etherPiggyBank = new EtherPiggyBank();
+        vm.stopBroadcast();
+    }
+}
