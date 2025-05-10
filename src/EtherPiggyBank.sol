ab2ee3884a419c13997c01479dbe5b426e307f99
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-10 11:11:19 +0100

    writing function

--- src/EtherPiggyBank.sol
+++ src/EtherPiggyBank.sol
@@ -3,68 +3,78 @@ pragma solidity ^0.8.19;
 
 contract EtherPiggyBank {
 
-
     address bankManager;
-
     address[] members;
 
-    mapping (address => uint) balance;
-
+    mapping(address => uint) balance;
     mapping(address => bool) registeredUsers;
 
-    constructor (){
+    // Custom errors
+    error OnlyBankManager();
+    error NotAMember();
+    error ZeroAddressNotAllowed();
+    error AlreadyMember();
+    error ManagerIsAlreadyMember();
+    error AmountMustBeGreaterThanZero();
+    error InsufficientFunds();
+    error TransferFailed();
+
+    constructor() {
         bankManager = msg.sender;
         members.push(msg.sender);
         registeredUsers[bankManager] = true;
     }
 
-    modifier onlyBankManager () {
-        require (bankManager == msg.sender, "Only bank manager can perform this action");
+    modifier onlyBankManager() {
+        if (msg.sender != bankManager) revert OnlyBankManager();
         _;
     }
 
     modifier onlyRegisteredMember() {
-        require(registeredUsers[msg.sender], "Must be a member");
+        if (!registeredUsers[msg.sender]) revert NotAMember();
         _;
     }
 
-    function deposit () public payable {
-        
-        
-        
+    function deposit() public payable {
+        // Optionally leave this or remove if unused
     }
 
-    function addMember (address addy) public onlyBankManager {
-        require(msg.sender != address(0), "Address zero not allowed");
-        require(msg.sender != bankManager, "Bank manager is a member");
-        require(!registeredUsers[addy], "is a member");
+    function addMember(address addy) public onlyBankManager {
+        if (addy == address(0)) revert ZeroAddressNotAllowed();
+        if (addy == bankManager) revert ManagerIsAlreadyMember();
+        if (registeredUsers[addy]) revert AlreadyMember();
+
         registeredUsers[addy] = true;
         members.push(addy);
     }
 
-    function getMembers() public view returns (address[] memory){
+    function getMembers() public view returns (address[] memory) {
         return members;
     }
 
-    function depositAmount () public onlyRegisteredMember payable{
-        require (msg.value > 0, "Amount must be greater than zero");
+    function depositAmount() public onlyRegisteredMember payable {
+        if (msg.value == 0) revert AmountMustBeGreaterThanZero();
         balance[msg.sender] += msg.value;
     }
 
-    function getBalanceByManager (address _member) public view onlyBankManager returns (uint256){
-       require(_member != address(0), "Address zero not allowed");
-       return  balance[_member];
+    function getBalanceByManager(address _member) public view onlyBankManager returns (uint256) {
+        if (_member == address(0)) revert ZeroAddressNotAllowed();
+        return balance[_member];
     }
 
-    function withdraw (uint256 _amount) public onlyRegisteredMember {
-        require(msg.sender != address(0), "Address zero not allowed");
+    function withdraw(uint256 _amount) public onlyRegisteredMember {
+        if (msg.sender == address(0)) revert ZeroAddressNotAllowed();
+        if (_amount == 0) revert AmountMustBeGreaterThanZero();
+        if (_amount > balance[msg.sender]) revert InsufficientFunds();
+
         (bool success, ) = msg.sender.call{value: _amount}("");
-        require(success, "Failed");
+        if (!success) revert TransferFailed();
 
+        balance[msg.sender] -= _amount;
     }
 
-    function getBalanceByUser () public view returns (uint256){
-       require(msg.sender != address(0), "Address zero not allowed");
-       return  balance[msg.sender];
+    function getBalanceByUser() public view returns (uint256) {
+        if (msg.sender == address(0)) revert ZeroAddressNotAllowed();
+        return balance[msg.sender];
     }
 }
